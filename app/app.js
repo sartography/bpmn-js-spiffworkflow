e46505e9a3fc0661438dc2298f9f65c09cc49d3a
Dan <daniel.h.funk@gmail.com>
2022-06-07 14:52:29 -0400

    Add IO Palette to display the input and output elements.

--- app/app.js
+++ app/app.js
@@ -1,20 +1,17 @@
 import BpmnModeler from 'bpmn-js/lib/Modeler';
-import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
 import diagramXML from '../resources/diagram.bpmn';
-// import customModule from './custom';
-// import qaExtension from '../resources/qa';
+import spiffworkflow from './spiffworkflow';
+const containerEl = document.getElementById('container');
 
-const HIGH_PRIORITY = 1500;
-
-const containerEl = document.getElementById('container')
 // create modeler
 const bpmnModeler = new BpmnModeler({
-    container: containerEl,
-    additionalModules: [
-        ],
-    moddleExtensions: {
-    }
+  container: containerEl,
+  additionalModules: [
+    spiffworkflow
+  ],
+  moddleExtensions: {}
 });
+
 
 // import XML
 bpmnModeler.importXML(diagramXML).then(() => {})
---
+++ app/spiffworkflow/index.js
@@ -0,0 +1,6 @@
+import IoPalette from './IoPalette';
+
+export default {
+  __init__: [ 'IoPalette' ],
+  IoPalette: [ 'type', IoPalette ],
+};
