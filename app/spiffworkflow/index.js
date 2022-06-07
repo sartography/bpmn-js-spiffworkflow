f1b98bcb706071d13d2a22f4e2a4cdd4cc94c90e
Dan <daniel.h.funk@gmail.com>
2022-06-07 15:08:47 -0400

    all the components for doing data/input and data/output

--- app/spiffworkflow/index.js
+++ app/spiffworkflow/index.js
@@ -1,6 +1,11 @@
 import IoPalette from './IoPalette';
+import IoRules from './IoRules';
+import IoInterceptor from './IoInterceptor';
 
 export default {
-  __init__: [ 'IoPalette' ],
+  __init__: [ 'IoPalette', 'IoRules', 'IoInterceptor' ],
   IoPalette: [ 'type', IoPalette ],
+  IoRules: [ 'type', IoRules ],
+  IoInterceptor: [ 'type', IoInterceptor ]
 };
+
