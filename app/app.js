bd9d530a503432ef0fd4d58fe37bb1e258af4d12
Dan <daniel.h.funk@gmail.com>
2022-09-21 13:44:10 -0400

    prep for release.

--- app/app.js
+++ app/app.js
@@ -62,15 +62,6 @@ bpmnModeler.on('spiff.service_tasks.requested', (event) => {
           },
         ],
       },
-      {
-        id: 'Fact about a Number',
-        parameters: [
-          {
-            id: 'number',
-            type: 'integer',
-          },
-        ],
-      },
     ],
   });
 });
