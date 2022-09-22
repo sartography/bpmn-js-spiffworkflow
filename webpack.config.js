3fc09c271dc749cfb1b9bb3103101ed6180bdc07
Dan <daniel.h.funk@gmail.com>
2022-09-22 10:36:00 -0400

    Clean up dependencies, and assure build / package process creates something that is easy to import and use.

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,74 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    bundle: ['./app/app.js']
-  },
-  output: {
-    path: __dirname + '/public',
-    filename: 'app.js'
-  },
-  module: {
-    rules: [
+const myModule = {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          plugins: [
+            [
+              '@babel/plugin-transform-react-jsx',
+              {
+                importSource: '@bpmn-io/properties-panel/preact',
+                runtime: 'automatic',
+              },
+            ],
+          ],
+        },
+      },
+    },
+    {
+      test: /\.bpmn$/,
+      use: 'raw-loader',
+    },
+  ],
+};
+
+const myPlugins = [
+  new CopyWebpackPlugin({
+    patterns: [
       {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              [ '@babel/plugin-transform-react-jsx', {
-                'importSource': '@bpmn-io/properties-panel/preact',
-                'runtime': 'automatic'
-              } ]
-            ]
-          }
-        }
+        from: 'assets/**',
+        to: 'vendor/bpmn-js',
+        context: 'node_modules/bpmn-js/dist/',
       },
       {
-        test: /\.bpmn$/,
-        use: 'raw-loader'
-      }
-    ]
+        from: 'assets/**',
+        to: 'vendor/bpmn-js-properties-panel',
+        context: 'node_modules/bpmn-js-properties-panel/dist/',
+      },
+      { from: '**/*.{html,css}', context: 'app/' },
+    ],
+  }),
+];
+
+module.exports = [
+  {
+    entry: {
+      bundle: ['./app/spiffworkflow'],
+    },
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'bpmn_spiffworkflow.js',
+    },
+    module: myModule,
+    mode: 'development',
+    devtool: 'source-map',
   },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'assets/**', to: 'vendor/bpmn-js', context: 'node_modules/bpmn-js/dist/' },
-        {
-          from: 'assets/**',
-          to: 'vendor/bpmn-js-properties-panel',
-          context: 'node_modules/bpmn-js-properties-panel/dist/'
-        },
-        {from: '**/*.{html,css}', context: 'app/'}
-      ]
-    })
-  ],
-  mode: 'development',
-  devtool: 'source-map'
-};
+  {
+    entry: {
+      bundle: ['./app/app.js'],
+    },
+    output: {
+      path: `${__dirname}/public`,
+      filename: 'app.js',
+    },
+    module: myModule,
+    plugins: myPlugins,
+    mode: 'development',
+    devtool: 'source-map',
+  },
+];
