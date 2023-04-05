6391337a86a6342fcfcfcd3e57d39cb61c7af668
Dan <daniel.h.funk@gmail.com>
2023-04-05 10:42:56 -0400

    Fix for lanes that doesn't break the world.

--- app/spiffworkflow/DataObject/DataObjectInterceptor.js
+++ app/spiffworkflow/DataObject/DataObjectInterceptor.js
@@ -46,11 +46,13 @@ export default class DataObjectInterceptor extends CommandInterceptor {
         }
       } else if (is(businessObject, 'bpmn:DataObject')) {
         // For data objects, only update the flowElements for new data objects, and set the parent so it doesn't get moved.
-        if (typeof(businessObject.$parent) === 'undefined') {
+        if (typeof (businessObject.$parent) === 'undefined') {
           const flowElements = realParent.get('flowElements');
           flowElements.push(businessObject);
           businessObject.$parent = realParent;
         }
+      } else {
+        bpmnUpdater.__proto__.updateSemanticParent.call(bpmnUpdater, businessObject, parentBusinessObject);
       }
     };
 
