7e5aaf0df01cfa0a78d556518622defd08ea66b8
Dan Funk <daniel.h.funk@gmail.com>
2024-04-03 09:49:28 -0400

    hooking message button to post and listen to the event bus.

--- app/spiffworkflow/messages/propertiesPanel/elementLevelProvider/MessageLaunchEditorButton.js
+++ app/spiffworkflow/messages/propertiesPanel/elementLevelProvider/MessageLaunchEditorButton.js
@@ -1,5 +1,6 @@
 import { HeaderButton } from '@bpmn-io/properties-panel';
 import { useService } from 'bpmn-js-properties-panel';
+import {getMessageRefElement} from "../../MessageHelpers";
 
 /**
  * Sends a notification to the host application saying the user
@@ -7,14 +8,32 @@ import { useService } from 'bpmn-js-properties-panel';
  * update the value and send it back.
  */
 export function MessageLaunchEditorButton(props) {
-    const { element, name, event, listenEvent, listenFunction } = props;
-    const eventBus = useService('eventBus');
-    return HeaderButton({
-        className: 'spiffworkflow-properties-panel-button',
-        id: `message_launch_editor_button_${name}`,
-        onClick: () => {
-            alert("Open message editor")
-        },
-        children: 'Open message editor',
-    });
+  const { element } = props;
+
+  const sendEvent = 'spiff.message.edit';
+  const listenEvent = 'spiff.message.update';
+
+  const eventBus = useService('eventBus');
+
+  const messageRef = getMessageRefElement(element);
+  let messageId = null;
+  if (messageRef && messageRef.id) {
+    messageId = messageRef.id;
+  }
+  return HeaderButton({
+    className: 'spiffworkflow-properties-panel-button',
+    id: `message_launch_message_editor_button`,
+    onClick: () => {
+      eventBus.fire(sendEvent, {
+        value: messageId,
+        eventBus,
+        listenEvent,
+      });
+      // Listen for a response, to update the script.
+      eventBus.once(listenEvent, (response) => {
+        messageRef.id = response.value;
+      });
+    },
+    children: 'Open message editor',
+  });
 }
