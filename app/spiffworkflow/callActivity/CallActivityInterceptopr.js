7e68511a7cca2dc1f5b6beaf2579777dcdcd5e93
Ayoub Ait Lachgar <44379029+theaubmov@users.noreply.github.com>
2024-04-16 15:39:22 +0100

    Add DrillUp to Callactivity element

--- app/spiffworkflow/callActivity/CallActivityInterceptopr.js
+++ app/spiffworkflow/callActivity/CallActivityInterceptopr.js
@@ -1,93 +1,45 @@
 import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
-import { getDi, is } from 'bpmn-js/lib/util/ModelUtil';
-import { remove as collectionRemove } from 'diagram-js/lib/util/Collections';
-
-const HIGH_PRIORITY = 1500;
+import { is } from 'bpmn-js/lib/util/ModelUtil';
 
 export default class CallActivityInterceptor extends CommandInterceptor {
 
-    constructor(eventBus, bpmnFactory, commandStack, bpmnUpdater, modeling, elementRegistry, overlays) {
+    constructor(eventBus, bpmnUpdater, overlays) {
         super(eventBus);
 
         bpmnUpdater.updateSemanticParent = (businessObject, parentBusinessObject) => {
             if (is(businessObject, 'bpmn:CallActivity')) {
-                console.log('updateSemanticParent', businessObject, parentBusinessObject, eventBus, bpmnFactory, commandStack, bpmnUpdater, modeling, overlays);
-                // modeling.updateProperties(businessObject, {
-                //     style: {
-                //         stroke: 'blue',
-                //         strokeWidth: 2
-                //     }
-                // });
-                overlays.add(
-                    businessObject.id,
-                    {
-                        // position: 'bottom-right',
-                        // // label: `${element.completeScope}`,
-                        // style: {
-                        //     font: { color: 'white', size: 16 },
-                        //     fill: { color: 'grey', opacity: 50 },
-                        //     // stroke: { color: 'grey', width: 2 },
-                        //     stroke: 'blue',
-                        // },
-                        // html: '<h1>dd</h1>'
-                        html: '<div>I am an overlay!</div>',
-                        position: {
-                          top: 0,
-                          right: 0
-                        },
-                        show: {
-                          minZoom: 0.8, // overlay will not be visible at zoom <0.8
-                          maxZoom: 1.2 // overlay will not be visible at zoom >1.2
-                        },
-                        scale: {
-                          min: 0.8, // overlay will not scale below 0.8
-                          max: 1.2 // overlay will not scale above 1.2
-                        }
+                eventBus.on('selection.changed', function (event) {
+                    var selectedElements = event.newSelection;
 
-                    },
-                );
+                    if (selectedElements.length > 0 && is(selectedElements[0], 'bpmn:CallActivity')) {
+                        const element = selectedElements[0];
 
+                        var ARROW_DOWN_SVG = '<svg width="20" height="20" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"> <g id="SVGRepo_bgCarrier" stroke-width="0"> <rect x="0" y="0" width="24.00" height="24.00" rx="0" fill="#2196f3" strokewidth="0"/> </g> <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"/> <g id="SVGRepo_iconCarrier"> <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#ffffff" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/> </g> </svg>';
+                        var button = domify('<button class="bjs-drilldown">' + ARROW_DOWN_SVG + '</button>');
+                        button.addEventListener('click', function () {
+                            alert('here');
+                        });
 
+                        overlays.add(element.id, 'drilldown', {
+                            position: {
+                                bottom: -10,
+                                right: -8
+                            },
+                            html: button
+                        });
+                    } else if (is(businessObject, 'bpmn:CallActivity') || is(selectedElements[0], 'bpmn:CallActivity')) {
+                        overlays.clear();
+                    }
+                });
             }
         };
-
-        this.preExecute(['shape.create'], HIGH_PRIORITY, function (event) {
-            const { context } = event;
-            const { shape } = context;
-            if (is(shape, 'bpmn:CallActivity')) {
-                console.log('preExecute shape.create');
-                event.stopPropagation();
-            }
-        });
-
-        /**
-         * Don't just create a new data object, use the first existing one if it already exists
-         */
-        this.executed(['shape.create'], HIGH_PRIORITY, function (event) {
-            const { context } = event;
-            const { shape } = context;
-            if (is(shape, 'bpmn:CallActivity')) {
-                console.log('executed shape.create');
-            }
-        });
-
-        this.postExecuted(['shape.create'], HIGH_PRIORITY, function (event) {
-            const { context } = event;
-            const { shape } = context;
-            if (is(shape, 'bpmn:CallActivity')) {
-                console.log('postExecuted shape.create');
-            }
-        });
-
-        this.executed(['shape.delete'], HIGH_PRIORITY, function (event) {
-            const { context } = event;
-            const { shape } = context;
-            if (is(shape, 'bpmn:CallActivity')) {
-                console.log('executed shape.delete');
-            }
-        });
-
     }
 }
 
-CallActivityInterceptor.$inject = ['eventBus', 'bpmnFactory', 'commandStack', 'bpmnUpdater', 'modeling', 'elementRegistry', 'overlays'];
+CallActivityInterceptor.$inject = ['eventBus', 'bpmnUpdater', 'overlays'];
+
+function domify(htmlString) {
+    const template = document.createElement('template');
+    template.innerHTML = htmlString.trim();
+    return template.content.firstChild;
+}
