ff0caea2e0f8ec65ae713d07332df0f6fb5009fa
Ayoub Ait Lachgar <44379029+theaubmov@users.noreply.github.com>
2024-04-16 15:56:28 +0100

    Add Call Action

--- app/spiffworkflow/callActivity/CallActivityInterceptopr.js
+++ app/spiffworkflow/callActivity/CallActivityInterceptopr.js
@@ -8,6 +8,7 @@ export default class CallActivityInterceptor extends CommandInterceptor {
 
         bpmnUpdater.updateSemanticParent = (businessObject, parentBusinessObject) => {
             if (is(businessObject, 'bpmn:CallActivity')) {
+                // Apply Custom overlay only when Callactivity element is selected
                 eventBus.on('selection.changed', function (event) {
                     var selectedElements = event.newSelection;
 
@@ -17,7 +18,11 @@ export default class CallActivityInterceptor extends CommandInterceptor {
                         var ARROW_DOWN_SVG = '<svg width="20" height="20" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"> <g id="SVGRepo_bgCarrier" stroke-width="0"> <rect x="0" y="0" width="24.00" height="24.00" rx="0" fill="#2196f3" strokewidth="0"/> </g> <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"/> <g id="SVGRepo_iconCarrier"> <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#ffffff" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/> </g> </svg>';
                         var button = domify('<button class="bjs-drilldown">' + ARROW_DOWN_SVG + '</button>');
                         button.addEventListener('click', function () {
-                            alert('here');
+                            const processId = getCalledElementValue(element);
+                            eventBus.fire('spiff.callactivity.edit', {
+                                element,
+                                processId,
+                            });
                         });
 
                         overlays.add(element.id, 'drilldown', {
@@ -34,12 +39,23 @@ export default class CallActivityInterceptor extends CommandInterceptor {
             }
         };
     }
-}
 
-CallActivityInterceptor.$inject = ['eventBus', 'bpmnUpdater', 'overlays'];
+    
+}
 
 function domify(htmlString) {
     const template = document.createElement('template');
     template.innerHTML = htmlString.trim();
     return template.content.firstChild;
 }
+
+function getCalledElementValue(element) {
+    const { calledElement } = element.businessObject;
+    if (calledElement) {
+        return calledElement;
+    }
+    return '';
+}
+
+CallActivityInterceptor.$inject = ['eventBus', 'bpmnUpdater', 'overlays'];
+
