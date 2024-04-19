417d28eb8519d5854a586260b8205daa7882dd1b
Ayoub Ait Lachgar <44379029+theaubmov@users.noreply.github.com>
2024-04-19 02:39:58 +0100

    Fix React Modules (#86)

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.m?jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -30,6 +30,9 @@ module.exports = {
         use: 'raw-loader'
       }
     ]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx']
   },
   plugins: [
     new CopyWebpackPlugin({
