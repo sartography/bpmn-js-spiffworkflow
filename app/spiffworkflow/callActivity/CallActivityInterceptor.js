c8135f5b3e1ec98ae820434655587b96efc781e8
theaubmov <ayoubaitlachgar98@gmail.com>
2024-04-25 14:31:14 +0100

    Fix multiple call activities issue

--- app/spiffworkflow/callActivity/CallActivityInterceptor.js
+++ app/spiffworkflow/callActivity/CallActivityInterceptor.js
@@ -1,50 +1,40 @@
-import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
 import { is } from 'bpmn-js/lib/util/ModelUtil';
 
-export default class CallActivityInterceptor extends CommandInterceptor {
+export default function CallActivityInterceptor(eventBus, bpmnUpdater, overlays) {
 
-    constructor(eventBus, bpmnUpdater, overlays) {
-        super(eventBus);
+    let OVERLAY_ID;
 
-        bpmnUpdater.updateSemanticParent = (businessObject, parentBusinessObject) => {
-            if (is(businessObject, 'bpmn:CallActivity')) {
-                // Apply Custom overlay only when Callactivity element is selected
-                eventBus.on('selection.changed', function (event) {
-                    var selectedElements = event.newSelection;
+    eventBus.on('selection.changed', function (event) {
 
-                    if (selectedElements.length > 0 && is(selectedElements[0], 'bpmn:CallActivity')) {
-                        const element = selectedElements[0];
+        var newSelection = event.newSelection;
+        const element = (newSelection.length > 0) ? newSelection[0] : null;
 
-                        var ARROW_DOWN_SVG = '<svg width="20" height="20" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"> <g id="SVGRepo_bgCarrier" stroke-width="0"> <rect x="0" y="0" width="24.00" height="24.00" rx="0" fill="#2196f3" strokewidth="0"/> </g> <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"/> <g id="SVGRepo_iconCarrier"> <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#ffffff" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/> </g> </svg>';
-                        var button = domify('<button class="bjs-drilldown">' + ARROW_DOWN_SVG + '</button>');
-                        button.addEventListener('click', function () {
-                            const processId = getCalledElementValue(element);
-                            if(!processId || processId === ''){
-                                alert('Please select a process model first');
-                                return;
-                            }
-                            eventBus.fire('spiff.callactivity.edit', {
-                                element,
-                                processId,
-                            });
-                        });
+        (OVERLAY_ID) ? overlays.remove(OVERLAY_ID) : null;
 
-                        overlays.add(element.id, 'drilldown', {
-                            position: {
-                                bottom: -10,
-                                right: -8
-                            },
-                            html: button
-                        });
-                    } else if (is(businessObject, 'bpmn:CallActivity') || is(selectedElements[0], 'bpmn:CallActivity')) {
-                        overlays.clear();
-                    }
+        if (element && is(element.businessObject, 'bpmn:CallActivity') && newSelection.length === 1) {
+            var ARROW_DOWN_SVG = '<svg width="20" height="20" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"> <g id="SVGRepo_bgCarrier" stroke-width="0"> <rect x="0" y="0" width="24.00" height="24.00" rx="0" fill="#2196f3" strokewidth="0"/> </g> <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"/> <g id="SVGRepo_iconCarrier"> <path d="M7 17L17 7M17 7H8M17 7V16" stroke="#ffffff" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/> </g> </svg>';
+            var button = domify('<button class="bjs-drilldown">' + ARROW_DOWN_SVG + '</button>');
+            button.addEventListener('click', function () {
+                const processId = getCalledElementValue(element);
+                if (!processId || processId === '') {
+                    alert('Please select a process model first');
+                    return;
+                }
+                eventBus.fire('spiff.callactivity.edit', {
+                    element,
+                    processId,
                 });
-            }
-        };
-    }
+            });
+            OVERLAY_ID = overlays.add(element.id, 'drilldown', {
+                position: {
+                    bottom: -10,
+                    right: -8
+                },
+                html: button
+            });
+        }
 
-    
+    });
 }
 
 function domify(htmlString) {
