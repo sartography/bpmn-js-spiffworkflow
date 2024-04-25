20d81fc8efadf17cffb0e7cd0a6050903978ac9f
Ayoub Ait Lachgar <44379029+theaubmov@users.noreply.github.com>
2024-04-25 17:23:22 +0100

    Go to call activity from process model task (#87)
    * Fix React Modules
    
    * Init
    
    * Add DrillUp to Callactivity element
    
    * Add Call Action
    
    * Add Message
    
    * Fix multiple call activities issue

--- app/spiffworkflow/callActivity/index.js
+++ app/spiffworkflow/callActivity/index.js
@@ -1,6 +1,8 @@
+import CallActivityInterceptor from './CallActivityInterceptor';
 import CallActivityPropertiesProvider from './propertiesPanel/CallActivityPropertiesProvider';
 
 export default {
   __init__: ['callActivityPropertiesProvider'],
   callActivityPropertiesProvider: ['type', CallActivityPropertiesProvider],
+  callActivityInterceptor: [ 'type', CallActivityInterceptor ],
 };
