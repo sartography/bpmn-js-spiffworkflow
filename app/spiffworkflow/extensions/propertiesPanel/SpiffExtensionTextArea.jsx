d2172577e48cc1590a0a6f4b7b76aa98ac06cf0f
burnettk <burnettk@users.noreply.github.com>
2024-12-30 22:43:51 -0500

    fix import and add all related files

--- app/spiffworkflow/extensions/propertiesPanel/SpiffExtensionTextArea.jsx
+++ app/spiffworkflow/extensions/propertiesPanel/SpiffExtensionTextArea.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useService } from 'bpmn-js-properties-panel';
-import { TextAreaEntry, TextFieldEntry } from '@bpmn-io/properties-panel';
+import {
+  TextAreaEntry,
+  TextFieldEntry,
+  isTextAreaEntryEdited,
+} from '@bpmn-io/properties-panel';
 import { getExtensionValue, setExtensionValue } from '../extensionHelpers';
 
 /**
