ea85c8ca678df77e3d6281ae9a2c649ca8d5e20e
Elizabeth Esswein <essweine@users.noreply.github.com>
2026-02-05 10:40:42 -0500

    Feature/validate data item ids (#140)
    * add validation to data objects
    
    * I realized that the request was for data inputs
    
    * minor fixes

--- app/spiffworkflow/InputOutput/index.js
+++ app/spiffworkflow/InputOutput/index.js
@@ -2,11 +2,13 @@ import IoPalette from './IoPalette';
 import IoRules from './IoRules';
 import IoInterceptor from './IoInterceptor';
 import IoPropertiesProvider from './propertiesProvider/IoPropertiesProvider';
+import DataInputOutputPropertiesProvider from './propertiesProvider/DataInputOutputPropertiesProvider';
 
 export default {
-  __init__: ['IoPalette', 'IoRules', 'IoInterceptor', 'IoPropertiesProvider'],
+  __init__: ['IoPalette', 'IoRules', 'IoInterceptor', 'IoPropertiesProvider', 'DataInputOutputPropertiesProvider'],
   IoPalette: ['type', IoPalette],
   IoRules: ['type', IoRules],
   IoInterceptor: ['type', IoInterceptor],
   IoPropertiesProvider: ['type', IoPropertiesProvider],
+  DataInputOutputPropertiesProvider: ['type', DataInputOutputPropertiesProvider],
 };
---
+++ app/spiffworkflow/InputOutput/propertiesProvider/DataInputOutputPropertiesProvider.js
@@ -0,0 +1,76 @@
+import { useService } from 'bpmn-js-properties-panel';
+import { isTextFieldEntryEdited, TextFieldEntry } from '@bpmn-io/properties-panel';
+import { useCallback } from '@bpmn-io/properties-panel/preact/hooks';
+import { validateDataId } from '../../helpers';
+
+const LOW_PRIORITY = 500;
+
+export default function DataInputOutputPropertiesProvider(
+  propertiesPanel,
+  translate,
+  moddle,
+  commandStack,
+  elementRegistry,
+  bpmnFactory
+) {
+  this.getGroups = function getGroupsCallback(element) {
+    return function pushGroup(groups) {
+      if (['bpmn:DataInput', 'bpmn:DataOutput'].includes(element.type)) {
+        const general = groups.find(group => group.id == 'general');
+        general.entries = general.entries.filter(entry => entry.id != 'id');
+        general.entries.unshift({
+          component: DataTextField,
+          isEdited: isTextFieldEntryEdited,
+          inputOrOutput: element.businessObject,
+        });
+      }
+      return groups;
+    };
+  };
+  propertiesPanel.registerProvider(LOW_PRIORITY, this);
+}
+
+DataInputOutputPropertiesProvider.$inject = [
+  'propertiesPanel',
+  'translate',
+  'moddle',
+  'commandStack',
+  'elementRegistry',
+  'bpmnFactory',
+];
+
+function DataTextField(props) {
+  const { element, inputOrOutput } = props;
+
+  const commandStack = useService('commandStack');
+  const debounce = useService('debounceInput');
+
+  const setValue = (value, error) => {
+    if (error)
+      return;
+
+    commandStack.execute('element.updateModdleProperties', {
+      element,
+      moddleElement: inputOrOutput,
+      properties: {
+        id: value,
+      },
+    });
+  };
+
+  const getValue = () => {
+    return inputOrOutput.id;
+  };
+
+  const validate = useCallback(value => validateDataId(value), [element]);
+
+  return TextFieldEntry({
+    element: element,
+    id: 'id',
+    label: 'ID',
+    getValue,
+    setValue,
+    debounce,
+    validate
+  });
+}
